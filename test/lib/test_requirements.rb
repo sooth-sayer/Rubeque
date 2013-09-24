974954224f617fb4e8c96cadeca41dcb3c3aadcb
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2013-09-24 18:55:29 +0400

    Unused files removed

--- test/lib/test_requirements.rb
+++
@@ -1,7 +0,0 @@
-require "bundler/setup"
-require "minitest/autorun"
-
-Bundler.require
-
-include Rubeque
-include MiniTest
--- test/test_18_substracting_out_the_sugar.rb
+++
@@ -1,9 +0,0 @@
-# Subtracting Out The Sugar
-require "./lib/test_requirements"
-
-class SubstractingOutTheSugarTest < Test
-  def test_substracting_out_the_sugar
-    assert_equal 2.+(2), 2 + 2
-    assert_equal 40.+(2), 42
-  end
-end
